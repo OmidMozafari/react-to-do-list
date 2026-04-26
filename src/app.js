292b2fc61a4dd6bf6e1e8847ad59920252a5aec1
OmidMozafari <omidmozafari444@gmail.com>
2026-04-26 22:32:26 +0430

    third commit, until now, I have completed most parts of the project just the remaining part is the day and night mode

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { type } from "@testing-library/user-event/dist/type"
+import { use, useState } from "react"
 
 export default function App(){
 
@@ -9,29 +10,52 @@ export default function App(){
     }
 
     const [listItem, setListItem] = useState([])
+   
 
     function handleListItem(){
         if(inputData === ""){
             window.alert("please enter something to the input")
             return
         }
-        setListItem(prev => [...prev, inputData])
+        setListItem(prev => [...prev, {text: inputData, checked: false}])
          setInputData("")
         
     }
+
+    function toggleItem(index){
+        setListItem(prev =>
+        prev.map((item, i) => {
+        if(i === index){
+        return { ...item, checked: !item.checked }
+        }        
+        return item
+        
+        })
+        )
+        }
 
     function deleteListItems(){
         return setListItem([])
     }
 
+     function deleteItem(index){
+        setListItem(prev => prev.filter((item, i) => i !== index) )
+    }
 
+
+    const [screenStatus, setScreenStatus] = useState("day")
+
+    function handleScreenStatus(){
+
+    }
 
  
 
     return <div className="container">
+        <DayOrNightMode />
         <Header />
         <Form handleInputData = {handleInputData} inputData = {inputData} handleListItem = {handleListItem} listItem = {listItem} />
-        <List listItem = {listItem}/>
+        <List listItem = {listItem} toggleItem = {toggleItem} deleteItem = {deleteItem}/>
         <Footer deleteListItems = {deleteListItems} />
     </div>
 }
@@ -61,16 +85,21 @@ export default function App(){
    
 }
 
-function List({listItem}){
+function List({listItem, toggleItem, deleteItem}){
+
+
+
     return <ul className="listItemContainer">
         {listItem.map((value, index) => {
             return <li className="listItems" key={index}>
             <label className="checkboxContainer">
-                <input type="checkbox" />
+                <input type="checkbox" onChange={() => toggleItem(index)} />
                 <span className="checkmark"></span>
-                {value}
+                {value.checked === false ? value.text : <span style={{textDecoration: "line-through", textDecorationColor: "white", textDecorationThickness: "2px" }}>
+                    {value.text}
+                    </span>}
             </label>
-            <button className="iconListItem">❌</button>
+            <button className="iconListItem" onClick={() => deleteItem(index)}>❌</button>
         </li>
         })}
     </ul>
@@ -80,4 +109,8 @@ function Footer({deleteListItems}){
     return <footer>
         <button onClick={deleteListItems}>Delete All</button>
     </footer>
+}
+
+function DayOrNightMode(){
+    return <button className="dayOrNightMode" onClick={handleScreenStatus}>🌞 Day</button>
 }
