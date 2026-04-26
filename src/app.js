d8273f31de1c9879c0cd3d05f806d34f1c3846e1
OmidMozafari <omidmozafari444@gmail.com>
2026-04-26 11:25:10 +0430

    Second commit, could made some new functionalities and layouts and design for the project

--- src/app.js
+++ src/app.js
@@ -1,42 +1,83 @@
+import { useState } from "react"
+
 export default function App(){
+
+    const [inputData , setInputData] = useState("")
+
+    function handleInputData (e){
+        setInputData(e.target.value)
+    }
+
+    const [listItem, setListItem] = useState([])
+
+    function handleListItem(){
+        if(inputData === ""){
+            window.alert("please enter something to the input")
+            return
+        }
+        setListItem(prev => [...prev, inputData])
+         setInputData("")
+        
+    }
+
+    function deleteListItems(){
+        return setListItem([])
+    }
+
+
+
+ 
+
     return <div className="container">
         <Header />
-        <Input/>
-        <List />
-        <Footer />
+        <Form handleInputData = {handleInputData} inputData = {inputData} handleListItem = {handleListItem} listItem = {listItem} />
+        <List listItem = {listItem}/>
+        <Footer deleteListItems = {deleteListItems} />
     </div>
 }
-function Header(){
+    function Header(){
     return <div className="Header">
         <h2>To-Do-List</h2>
         <div className="icon">📝</div>
     </div>
 }
 
-function Input(){
-    return <div className="inputContainer">
-         <input placeholder="Add your text"></input>
-         <button>Add</button>
-    </div> 
+    function Form({handleInputData, handleSubmit, inputData, handleListItem}){
+
+       function handleSubmit(e){
+        e.preventDefault()
+        
+    }
+
+
+    return <form className="inputContainer" onSubmit={handleSubmit}>
+         <input placeholder="Add your text" onChange={handleInputData} value={inputData} ></input>
+         <small className="inputSmalMessage" style={{visibility: inputData === "" ? "visible" : "hidden"}}>
+            Please add something to the input
+            </small>
+
+         <button onClick={handleListItem}>Add</button>
+    </form> 
    
 }
 
-function List(){
+function List({listItem}){
     return <ul className="listItemContainer">
-
-   <li className="listItems">
+        {listItem.map((value, index) => {
+            return <li className="listItems" key={index}>
             <label className="checkboxContainer">
                 <input type="checkbox" />
                 <span className="checkmark"></span>
-                {/* Get the value from input and add it here*/}
+                {value}
             </label>
             <button className="iconListItem">❌</button>
         </li>
-
+        })}
     </ul>
+      
 }
-function Footer(){
+function Footer({deleteListItems}){
     return <footer>
-        <button>Delete All</button>
+        <button onClick={deleteListItems}>Delete All</button>
     </footer>
 }
